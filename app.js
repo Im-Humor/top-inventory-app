913391912b373f1176419758a9ea2d7ba4dae48b
Im-Humor <orangite@gmail.com>
2023-12-18 20:49:53 -0700

    set start command script, add the ability to render ejs view pages, create initial index view, add routing functionality to app

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const express = require("express")
-const app = express()
+// require modules
+const express = require("express");
+const app = express();
+
+//require routers
+const indexRouter = require("./routes/index");
+
+//set view engine to ejs
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+//start server on port 3000
+app.listen(3000);
+
+//load router files
+app.use("/", indexRouter);
