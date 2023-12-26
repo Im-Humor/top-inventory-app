06bac5cc36901c6fb943e13750f3a54c3c2a6bef
Im-Humor <orangite@gmail.com>
2023-12-26 12:12:33 -0700

    add mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // require modules
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+//set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.gfyczkq.mongodb.net/inventory-app?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect(mongoDB);
+}
+console.log(
+	`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.gfyczkq.mongodb.net/inventory-app?retryWrites=true&w=majority`
+);
+console.log(mongoose.connection.readyState);
 
 //require routers
 const indexRouter = require("./routes/index");
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
 	res.render("index", { title: "Homepage" });
 });
 
+router.get("/hello", (req, res) => {
+	res.render("test", { title: "Homepage" });
+});
+
 module.exports = router;
