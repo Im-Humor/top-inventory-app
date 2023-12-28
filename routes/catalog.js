769ad31eaf9ae358177d25c90f56eca58f07fc37
Im-Humor <orangite@gmail.com>
2023-12-28 10:06:51 -0700

    add category and item views

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,29 +4,56 @@ const mongoose = require("mongoose");
 const Item = require("../models/item");
 const Category = require("../models/category");
 
-//get list of category objects
-let categoryList = [];
-async function getCategories() {
-	categoryList = await Category.find({}).exec();
-}
-getCategories();
-
 //get list of item objects
-let itemList = [];
-async function getItems() {
-	itemList = await Item.find({}).exec();
-}
-getItems();
 
 router.get("/", (req, res) => {
 	res.render("index", { title: "Homepage" });
 });
 
 router.get("/categories", (req, res) => {
-	res.render("all_categories", {
-		title: "Categories",
-		categories: categoryList,
-	});
+	async function getCategories() {
+		let categoryList = await Category.find({}).exec();
+		res.render("all_categories", {
+			title: "Categories",
+			categories: categoryList,
+		});
+	}
+	getCategories();
+});
+
+router.get("/items", (req, res) => {
+	async function getItems() {
+		let itemList = await Item.find({}).exec();
+		res.render("all_items", {
+			title: "Items",
+			items: itemList,
+		});
+	}
+	getItems();
+});
+
+router.get("/category/:categoryID", (req, res) => {
+	async function getCategory() {
+		let searchCategory = await Category.findById(
+			req.params.categoryID
+		).exec();
+		res.render("view_category", {
+			title: searchCategory.name,
+			category: searchCategory,
+		});
+	}
+	getCategory();
+});
+
+router.get("/item/:itemID", (req, res) => {
+	async function getItem() {
+		let searchItem = await Item.findById(req.params.itemID).exec();
+		res.render("view_item", {
+			title: searchItem.name,
+			item: searchItem,
+		});
+	}
+	getItem();
 });
 
 module.exports = router;
