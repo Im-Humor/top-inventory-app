56e6084fe651a07e594c5079984759ec9b4f83c7
Im-Humor <orangite@gmail.com>
2023-12-29 10:29:00 -0700

    add delete and update functionalities

--- routes/catalog.js
+++ routes/catalog.js
@@ -37,9 +37,11 @@ router.get("/category/:categoryID", (req, res) => {
 		let searchCategory = await Category.findById(
 			req.params.categoryID
 		).exec();
+		let categoryItems = await Item.find({ category: searchCategory._id });
 		res.render("view_category", {
 			title: searchCategory.name,
 			category: searchCategory,
+			items: categoryItems,
 		});
 	}
 	getCategory();
@@ -71,6 +73,53 @@ router.post("/categories/new", (req, res) => {
 		res.render("new_category", { title: "Add New Category" });
 	}
 	makeNewCategory();
+});
+
+router.get("/category/:categoryID/delete", (req, res) => {
+	async function renderDeleteCategory() {
+		console.log(req.body);
+		const deleteCategory = await Category.findById(req.params.categoryID);
+		res.render("delete_category", {
+			title: "Delete Category",
+			category: deleteCategory,
+		});
+	}
+	renderDeleteCategory();
+});
+
+router.post("/category/:categoryID/delete", (req, res) => {
+	async function deleteCategory() {
+		const deleted = await Category.deleteOne({
+			_id: req.params.categoryID,
+		});
+		console.log(deleted);
+		res.redirect("/catalog/categories");
+	}
+	deleteCategory();
+});
+
+router.get("/category/:categoryID/edit", (req, res) => {
+	async function renderEditCategory() {
+		const category = await Category.findById(req.params.categoryID);
+		res.render("edit_category", {
+			title: "Edit Category",
+			category: category,
+		});
+	}
+	renderEditCategory();
+});
+
+router.post("/category/:categoryID/edit", (req, res) => {
+	async function editCategory() {
+		const category = await Category.findById(req.params.categoryID);
+		const update = await Category.updateOne(
+			{ _id: category._id },
+			{ name: req.body.name, desc: req.body.desc }
+		);
+		console.log(update);
+		res.redirect("/catalog/categories");
+	}
+	editCategory();
 });
 
 module.exports = router;
